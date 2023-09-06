ae1b045f7717831fc42f118ee9bf01fe138a6da3
sopansagorkar <sagorkars@gmail.com>
2023-09-06 21:58:54 +0530

    created tests for saucelab demo website

--- src/e2e/example.spec.js
+++ src/e2e/example.spec.js
@@ -2,18 +2,17 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.saucedemo.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Swag Labs/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.saucedemo.com/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.getByPlaceholder("Username").fill("standard_user");
+  await page.getByPlaceholder("Password").fill("secret_sauce");
+  await page.locator('input#login-button').click();
 });
