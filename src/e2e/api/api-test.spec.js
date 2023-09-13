8c21dc16b25d30c95223efaab56ae4e99832760c
sopansagorkar <sagorkars@gmail.com>
2023-09-13 22:29:32 +0530

    Added test for post request

--- src/e2e/api/api-test.spec.js
+++ src/e2e/api/api-test.spec.js
@@ -1,7 +1,12 @@
 const { test, expect } = require('@playwright/test');
-
+import postData from '../data/api-data.json';
 
 test('@get verify get request', async ({ request }) => {
     const response= await request.get('/booking/1');
+    console.log(response.json());
+  });
+
+  test('@post verify get request', async ({ request }) => {
+    const response= await request.post('/booking',{data:postData});
     console.log(response.json());
   });
