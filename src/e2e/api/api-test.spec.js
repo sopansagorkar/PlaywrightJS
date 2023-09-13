cd5fda411f58eed2266f0e3cba35bab74133aa9d
sopansagorkar <sagorkars@gmail.com>
2023-09-13 22:17:59 +0530

    Added JSON data file

--- src/e2e/api/api-test.spec.js
+++ src/e2e/api/api-test.spec.js
@@ -1,4 +1,6 @@
 const { test, expect } = require('@playwright/test');
+
+
 test('@get verify get request', async ({ request }) => {
     const response= await request.get('/booking/1');
     console.log(response.json());
