fddba3d760ed9b0a0b584e89165416f5d656da49
sopansagorkar <sagorkars@gmail.com>
2023-11-29 22:58:49 +0530

    Removed unused code

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### This POC is having all basic concepts for PlayWright using JS
 
 
-### ** Execute your test using below commands
+### Execute your test using below commands
 
 `npm run test:dev` to run all scripts
 `npm run test:dev src/e2e/specs/login.spec.js` to run specific script
--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ const getConfig = (env) => {
   }
 };
 
-const config=getConfig(environment);
+const config = getConfig(environment);
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -34,47 +34,17 @@ module.exports = defineConfig({
   reporter: [["html", { open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    screenshot: "on",
+    screenshot: "only-on-failure",
     baseURL: config.baseURL,
     // Record trace only when retrying a test for the first time.
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
 
     // Record video only when retrying a test for the first time.
-    video: "on",
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'],ignoreHTTPSErrors:true },
-    // },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
     {
       name: "Google Chrome",
       use: {
@@ -89,11 +59,4 @@ module.exports = defineConfig({
       },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
